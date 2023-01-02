07f08e0e4f349539d3d9efd972af12d6b79f4dbd
nima <nekoueinima@protonmail.com>
2023-01-02 14:27:22 +0330

    [Feat]: add hamber menu and refactor styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
-import { Roboto_Mono } from "@next/font/google";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../src/components/layout/Layout";
-
-const robotoMono = Roboto_Mono({
-  weight: ["400", "700"],
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/components/layout/Header/Navbar.tsx
+++ src/components/layout/Header/Navbar.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import styles from "./Navbar.module.scss";
 import { useRouter } from "next/router";
 import ColorMode from "../../ColorMode/ColorMode";
+import { useState } from "react";
 
 const Navbar = () => {
   const router = useRouter();
+  const [isMenuActive, setIsMenuActive] = useState(false);
 
   return (
     <header className={styles.header}>
@@ -12,18 +14,50 @@ const Navbar = () => {
         <h3>
           Nima <span>Nekouei</span>
         </h3>
-        <ul>
-          <li className={router.pathname == "/" ? styles.active : ""}>
-            <Link href="/">Home</Link>
-          </li>
-          <li className={router.pathname == "/resume" ? styles.active : ""}>
-            <Link href="/resume">Resume</Link>
-          </li>
-          <li className={router.pathname == "/contact" ? styles.active : ""}>
-            <Link href="/contact">Contact Me</Link>
-          </li>
+        <div>
+          <ul className={styles.desktopNav}>
+            <li className={router.pathname == "/" ? styles.active : ""}>
+              <Link href="/">Home</Link>
+            </li>
+            <li className={router.pathname == "/resume" ? styles.active : ""}>
+              <Link href="/resume">Resume</Link>
+            </li>
+            <li className={router.pathname == "/contact" ? styles.active : ""}>
+              <Link href="/contact">Contact Me</Link>
+            </li>
+          </ul>
+          <div className={styles.mobileNav}>
+            <button
+              onClick={(e) => setIsMenuActive(!isMenuActive)}
+              className={`${styles.hamburger} ${styles["hamburger--spin"]} ${
+                isMenuActive ? styles["is-active"] : ""
+              }`}
+              type="button"
+            >
+              <span className={styles["hamburger-box"]}>
+                <span className={styles["hamburger-inner"]}></span>
+              </span>
+            </button>
+            <ul
+              className={`${styles["mobileNav--list"]} ${
+                isMenuActive ? styles["is-active"] : ""
+              }`}
+            >
+              <li className={router.pathname == "/" ? styles.active : ""}>
+                <Link href="/">Home</Link>
+              </li>
+              <li className={router.pathname == "/resume" ? styles.active : ""}>
+                <Link href="/resume">Resume</Link>
+              </li>
+              <li
+                className={router.pathname == "/contact" ? styles.active : ""}
+              >
+                <Link href="/contact">Contact Me</Link>
+              </li>
+            </ul>
+          </div>
           <ColorMode />
-        </ul>
+        </div>
       </nav>
     </header>
   );
