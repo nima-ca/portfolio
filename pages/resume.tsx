95b69b01f83fb20ee2f3812df469a54504360020
nima <nekoueinima@protonmail.com>
2023-01-02 16:51:03 +0330

    [Feat]: change my resume page introduction text

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -9,14 +9,24 @@ const ResumePage = () => {
           Hi, I'm <span>Nima</span>
         </h3>
         <p className={styles.paragraph}>
-          React Front-End developer and computer engineering student currently
-          attending Najafabad University. Experienced in developing user
-          interfaces and web applications. Aiming to successfully fill in the
-          front-end developer role and work in conjunction with the other team
-          members. Eager to learn new Techs and proficient in an assortment of
-          technologies including JavaScript and TypeScript, and able to write
-          high-quality, clean, and tested code using patterns and best
-          practices!
+          I'm a 21-year-old software engineer. I'm from Iran and participating
+          at Najafabad university. I specialize in TypeScript, React, and NextJs
+          and I love making complex, beautiful, and performant web applications
+          and solving computer science problems.
+        </p>
+
+        <p className={styles.paragraph}>
+          I started to become a software engineer in January 2022, and I've had
+          the opportunity to work with a B2B company, which has been an amazing
+          learning experience for me, both in terms of technology and business.
+          And also, I have been focusing more on developer relations to improve
+          my people skills and get out of my coding comfort zone.
+        </p>
+
+        <p className={styles.paragraph}>
+          In my free time, I like to challenge myself to be more creative and
+          improve my tech skills by participating in coding meetings and
+          listening to tech-related podcasts.
         </p>
         <a
           className="button"
